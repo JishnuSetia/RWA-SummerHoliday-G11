08a4294e0204b39cdd24b6466272338d0301b8e6
Jishnu Setia <jishnusetia8@gmail.com>
2023-07-10 10:04:13 +0400

    Commit 2

--- PseudoArithmeticSequence.java
+++ PseudoArithmeticSequence.java
@@ -1,6 +1,15 @@
 public class PseudoArithmeticSequence {
     public static void main(String[] args) {
         int[] array = {2,5,6,8,9,12};
+        for(int i = 0;i<array.length-1;i++){
+            for(int j = 0;j<array.length-1-i;j++){
+                if(array[j]>array[j+1]){
+                    int tmp = array[j];
+                    array[j] = array[j+1];
+                    array[j+1] = tmp;
+                }
+            }
+        }
         int left=1,right=array.length-2;
             boolean chk=true;
             while(left<=right){
